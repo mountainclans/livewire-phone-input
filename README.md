b596776ed222f3c06833e661e8c001a96f36a2dc
Vladimir Bajenov <vladimir@bajenov.info>
2025-07-03 16:47:30 +0400

    README.md: basic installation instructions

--- README.md
+++ README.md
@@ -8,6 +8,31 @@
 composer require mountainclans/livewire-phone-input
 ```
 
+Поскольку функциональность пакета основана на [пакете Intl Tel Input](https://github.com/jackocnr/intl-tel-input), установите его через npm:
+
+```bash
+npm install intl-tel-input --save
+```
+
+**Обязательно** опубликуйте ассеты:
+```bash
+php artisan vendor:publish --tag=livewire-phone-input-assets
+```
+
+Добавьте в файл проекта `resources/js/app.js` строки
+
+```js
+import intlTelInput from 'intl-tel-input/intlTelInputWithUtils';
+import phoneInput from '/vendor/mountainclans/livewire-phone-input/resources/js/phoneInput'
+```
+
+Добавьте в файл проекта `resources/js/app.css` строку
+
+```
+@import 'intl-tel-input/build/css/intlTelInput.css';
+```
+
+
 Опционально, вы можете опубликовать view командой
 
 ```bash
@@ -18,7 +43,9 @@ php artisan vendor:publish --tag="livewire-phone-input-views"
 
 ## Авторы
 
-- [Vladimir Bajenov](https://github.com/mountainclans)
+- [Vladimir Bazhenov](https://github.com/mountainclans)
+- [Victory Osayi](https://github.com/victorybiz/laravel-tel-input)
+- [Jack O'Connor](https://github.com/jackocnr/intl-tel-input)
 - [All Contributors](../../contributors)
 
 ## Лицензия
---
+++ resources/views/phone-input.blade.php
@@ -0,0 +1,88 @@
+@props([
+     'label' => null,
+     'name',
+     'id',
+])
+
+@php
+    $wire = '';
+    foreach ($attributes->getAttributes() as $key => $value) {
+        if (str_contains($key, 'wire:model')) {
+            $wire = $value;
+            break;
+        }
+    }
+@endphp
+
+<div class="w-full">
+    @if ($label)
+        <div class="flex justify-between items-center">
+            <label for="{{ $name }}"
+                   class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >{{ $label }}</label>
+        </div>
+    @endif
+
+    {{-- Hidden phone input --}}
+    <input type="hidden"
+           id="{{ $id }}"
+           name="{{ $name }}"
+           @if ($attributes->whereStartsWith('wire:model')->first())
+               {{ $attributes->wire('model') }}
+           @endif
+           @if ($attributes->has('value'))
+               value="{{ $attributes->get('value') }}"
+           @endif
+           autocomplete="off"
+    />
+
+    {{-- Tel input --}}
+    <div class="w-full" wire:ignore>
+        <input type="tel"
+               class="iti--laravel-tel-input bg-gray-50 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 {{ $attributes->get('class') }}"
+               data-phone-input-id="{{ $id }}"
+               data-phone-input-name="{{ $name }}"
+               data-phone-input="#{{ $id }}"
+               @if ($attributes->has('value'))
+                   value="{{ $attributes->get('value') }}"
+               @endif
+               @if ($attributes->has('phone-country-input'))
+                   data-phone-country-input="#{{ $attributes->get('phone-country-input') }}"
+               @else
+                   @dd($attributes->all())
+               @endif
+               @if ($attributes->has('placeholder'))
+                   placeholder="{{ $attributes->get('placeholder') }}"
+               @endif
+               @if ($attributes->has('required'))
+                   required
+               @endif
+               @if ($attributes->has('disabled'))
+                   disabled
+               @endif
+               autocomplete="off"
+        />
+    </div>
+
+    @if ($attributes->has('phone-country-input'))
+        <input type="hidden"
+               id="{{ $attributes->get('phone-country-input') }}"
+               wire:model="{{ $attributes->get('phone-country-input') }}"
+        />
+
+        @error ($attributes->get('phone-country-input'))
+        <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $message }}</p>
+        @enderror
+    @endif
+
+    @once
+        <script>
+            document.dispatchEvent(new Event('telDOMChanged'));
+        </script>
+    @endonce
+
+    @error ($wire)
+    <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $message }}</p>
+    @enderror
+
+</div>
