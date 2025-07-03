87b308dc6f5eacb50582b20851c8cd5e01ed6954
Vladimir Bajenov <vladimir@bajenov.info>
2025-07-03 16:21:53 +0400

    Initial config

---
+++ config/livewire-phone-input.php
@@ -0,0 +1,6 @@
+<?php
+
+// config for MountainClans/LivewirePhoneInput
+return [
+
+];
---
+++ src/LivewirePhoneInputServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MountainClans\LivewirePhoneInput;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class LivewirePhoneInputServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        $package
+            ->name('livewire-phone-input')
+            ->hasConfigFile()
+            ->hasViews('ui');
+    }
+}
